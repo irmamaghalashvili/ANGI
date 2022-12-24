3b5db4c982668532b3016ade4243964a9dbdfbf6
irmamaghalashvili <imaghalashvili@gmail.com>
2022-12-24 20:00:16 +0400

    2

--- java.js
+++ java.js
@@ -1,6 +1,8 @@
 let nav = document.getElementById('navigations');
 let burgerBar = document.getElementById('burger');
+let header = document.getElementById('header');
 
 burgerBar.addEventListener('click', function() {
     nav.classList.toggle('activeNav')
+    header.classList.toggle('burgerHeader')
 });
